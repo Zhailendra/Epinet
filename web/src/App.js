541207d85e7034748ef113fb983e936364b99534
Zhailendra <shailendrablondel@outlook.fr>
2024-01-14 21:15:43 +0100

    [+] Login page init

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,10 @@
-
 import Login from "./pages/Login";
 
 export default function App() {
+
     return (
         <>
-          <Login />
+            <Login />
         </>
     );
 }
--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -1,9 +1,84 @@
 import React from 'react';
+import styles from '../styles/login.module.scss';
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login</h2>
+        <div className={styles.container}>
+            <div className={styles.loginBox}>
+                <h2>Login</h2>
+                <form action="#">
+                    <div className={styles.inputBox}>
+                        <input type="email" required />
+                            <label>Email</label>
+                    </div>
+                    <div className={styles.inputBox}>
+                        <input type="password" required />
+                            <label>Password</label>
+                    </div>
+                    <div className={styles.forgotPassword}>
+                        <a href="#">Forgot Password?</a>
+                    </div>
+                    <button type="submit" className={styles.button}>
+                        Login
+                    </button>
+                    <div className={styles.signup}>
+                        <span>Don't have an account?</span>
+                        <a href="#">Signup</a>
+                    </div>
+                </form>
+            </div>
+
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+            <span ></span>
+
         </div>
     );
 };
